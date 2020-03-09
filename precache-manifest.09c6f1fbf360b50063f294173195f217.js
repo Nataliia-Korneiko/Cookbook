22b91caf1aaa28f7ce390b5daaae283615b306a4
Nataliia Korneiko <korneyko_ns@ukr.net>
2020-03-09 13:19:05 +0200

    Updates

---
+++ precache-manifest.09c6f1fbf360b50063f294173195f217.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4a77146df29df026617bb17e96465f",
+    "url": "/cookbook/index.html"
+  },
+  {
+    "revision": "330dcbfb4fddcc7ca402",
+    "url": "/cookbook/static/css/main.5f04df60.chunk.css"
+  },
+  {
+    "revision": "bf050fbbc25cf7bd8045",
+    "url": "/cookbook/static/js/2.058ad22d.chunk.js"
+  },
+  {
+    "revision": "330dcbfb4fddcc7ca402",
+    "url": "/cookbook/static/js/main.aa45b0cd.chunk.js"
+  },
+  {
+    "revision": "997e80306bc6b0c74c68",
+    "url": "/cookbook/static/js/runtime-main.7239045e.js"
+  },
+  {
+    "revision": "a3fa42780a301e4eafa5fe6994495410",
+    "url": "/cookbook/static/media/backdrop-01.a3fa4278.jpg"
+  },
+  {
+    "revision": "bd78375e29f70750d4ac201e9721aba1",
+    "url": "/cookbook/static/media/backdrop-02.bd78375e.jpg"
+  },
+  {
+    "revision": "8481dad297c352715e1f320af1183006",
+    "url": "/cookbook/static/media/backdrop-03.8481dad2.png"
+  },
+  {
+    "revision": "d543f4ce2f65dfcf64b541e95e28432a",
+    "url": "/cookbook/static/media/chef-01.d543f4ce.png"
+  },
+  {
+    "revision": "112efea77224aaee6ab8ce9982d1440b",
+    "url": "/cookbook/static/media/dancing-script-v14-latin-700.112efea7.woff"
+  },
+  {
+    "revision": "9bbebf5d89c861f17ad08456b790c7b8",
+    "url": "/cookbook/static/media/dancing-script-v14-latin-700.9bbebf5d.woff2"
+  },
+  {
+    "revision": "014c7f030ef0c10e1cbdd3b3e6f277ae",
+    "url": "/cookbook/static/media/dancing-script-v14-latin-regular.014c7f03.woff"
+  },
+  {
+    "revision": "5c1198f377ebf0fe64dd1a42476f0309",
+    "url": "/cookbook/static/media/dancing-script-v14-latin-regular.5c1198f3.woff2"
+  },
+  {
+    "revision": "4ee09a92e70ae085a8325f1c506cc857",
+    "url": "/cookbook/static/media/marck-script-v10-cyrillic-regular.4ee09a92.woff"
+  },
+  {
+    "revision": "6949c96bf4dc1939df228292045db43e",
+    "url": "/cookbook/static/media/marck-script-v10-cyrillic-regular.6949c96b.woff2"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/cookbook/static/media/montserrat-v14-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/cookbook/static/media/montserrat-v14-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/cookbook/static/media/montserrat-v14-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/cookbook/static/media/montserrat-v14-latin-regular.bc3aa95d.woff2"
+  }
+]);
